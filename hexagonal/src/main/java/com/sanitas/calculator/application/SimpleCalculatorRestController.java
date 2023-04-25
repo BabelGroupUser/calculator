3674a63c315e06e159f2680b5437cc3e6ceba616
francisco.gbarrero <francisco.gbarrero@babelgroup.com>
2023-04-25 14:28:36 +0200

    Changed Rest Controller to use lombok for the constructor

--- hexagonal/src/main/java/com/sanitas/calculator/application/SimpleCalculatorRestController.java
+++ hexagonal/src/main/java/com/sanitas/calculator/application/SimpleCalculatorRestController.java
@@ -4,9 +4,9 @@ import com.sanitas.calculator.application.mapper.OperationTermsMapper;
 import com.sanitas.calculator.controllers.DefaultApiDelegate;
 import com.sanitas.calculator.domain.IntegerCalculatorService;
 import com.sanitas.calculator.domain.OperationTerms;
+import lombok.RequiredArgsConstructor;
 import org.openapitools.model.SimpleCalculatorRequest;
 import org.openapitools.model.SimpleCalculatorResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/simplecalculator")
 public class SimpleCalculatorRestController implements DefaultApiDelegate {
     private NativeWebRequest request;
@@ -26,15 +27,6 @@ public class SimpleCalculatorRestController implements DefaultApiDelegate {
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
-    }
-
-    @Autowired
-    public SimpleCalculatorRestController(
-            IntegerCalculatorService integerCalculatorService,
-            OperationTermsMapper mapper
-    ) {
-        this.integerCalculatorService = integerCalculatorService;
-        this.mapper = mapper;
     }
 
     @Override
